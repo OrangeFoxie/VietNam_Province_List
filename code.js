eca71afca5ecdee467c643f3af7f2c767155ddf1
Orange Foxie <vinhphuc931998@gmail.com>
2021-07-13 23:17:59 +0700

    get data

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 const api = 'https://provinces.open-api.vn/api/?depth=3';
+const cityArr = new Array();
+const distArr = new Array();
 
 async function fetchAsync () {
     let response = await fetch(api);
@@ -7,30 +9,52 @@ async function fetchAsync () {
   }
 
     fetchAsync()
-        .then(data => 
-            console.log(data)+
-            main(data))              // if(data) => print data
+        .then(data => window.onload = main(data))       // if(data) => print data
         .catch(reason => console.log(reason.message)) // if(!data) => print error
 
     function main(data){
-        getCity(data);
+        getDist(data);
+
         listProvinces(data);
+        listDistricts(data,distArr);
     }
     
     function listProvinces(data) {
-        let temp = "";
-    
+        let temp = "";    
         data.forEach((itemData) => {
             temp += `<tr>`;
-            temp += `<td>${itemData}</td>`;
+            temp += `<td>${itemData['name']}</td>`;
+            temp += `<td>${itemData['code']}</td>`;
+            temp += `<td>${itemData['codename']}</td>`;
+            temp += `<td>${itemData['division_type']}</td>`;
+            temp += `<td>${itemData['phone_code']}</td>`;
             temp += `</tr>`;
         });
         document.getElementById('tablelist').innerHTML = temp;
     }
 
-    const cityArr = new Array();
-    function getCity(data){
-        data.forEach(element => {
-            cityArr.push(element['districts','name'])});
-        console.log(cityArr);
+    function listDistricts(data,distArr) {
+        let temp = "";    
+
+        for(var k=0; k<data.length; k++){
+            for(var i=0; i<distArr.length; i++){
+                for(var j=0; j<distArr[i].length; j++){
+                    temp += `<tr>`;
+                    temp += `<td>${data[i]['name']}</td>`;
+                    temp += `<td>${distArr[i][j]['name']}</td>`;
+                    temp += `<td>${distArr[i][j]['code']}</td>`;
+                    temp += `<td>${distArr[i][j]['codename']}</td>`;
+                    temp += `<td>${distArr[i][j]['division_type']}</td>`;
+                    temp += `<td>${distArr[i][j]['short_codename']}</td>`;
+                    temp += `</tr>`; 
+                }           
+            }
+        }
+        document.getElementById('distlist').innerHTML = temp;
+    }
+
+    function getDist(data){
+        for(var i=0; i<data.length; i++){
+            distArr.push(data[i]['districts']);
+        }
     }
